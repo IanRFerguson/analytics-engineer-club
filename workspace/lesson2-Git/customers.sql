ac17e31388533d56730521dc9fd1a0d9dfae30df
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-10-12 12:56:52 +0000

    Add customers query

---
+++ workspace/lesson2-Git/customers.sql
@@ -0,0 +1,31 @@
+with customers as (
+  select
+    id,
+    name,
+    email
+  from 
+    `analytics-engineers-club.coffee_shop.customers`
+),
+
+orders as (
+  select
+    customer_id,
+    created_at
+  from
+    `analytics-engineers-club.coffee_shop.orders`
+)
+
+select
+  o.customer_id,
+  c.name,
+  c.email,
+  min(o.created_at) as first_order_at,
+  count(*) as number_of_orders
+from customers c
+left join orders o
+  on c.id = o.customer_id
+group by
+  1,2,3
+order by
+  first_order_at;
+
