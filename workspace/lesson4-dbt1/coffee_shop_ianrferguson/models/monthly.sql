6eb5fe134a1c41c83557b787a21164cad0f9d1a9
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-11-02 12:55:12 +0000

    added dbt sourcing

--- workspace/lesson4-dbt1/coffee_shop_ianrferguson/models/monthly.sql
+++ workspace/lesson4-dbt1/coffee_shop_ianrferguson/models/monthly.sql
@@ -2,5 +2,5 @@
 select
     date_trunc(first_order_at, month) as first_month,
     count(*) as total_customers
-from {{ ref('customers') }}
+from {{ source('customer_test', 'customers') }}
 group by 1
